c3bb3a21ae334f005dc9791c36eed42d3192c61a
SOPLAY <tkdgns1341@gmail.com>
2023-08-06 01:02:45 +0900

    Fix: todo api 수정
    - todo가 존재하지 않아도 get요청시 status code 200을 반환하는 오류 수정
    - router 작성 방식 변경

--- pages/api/todo/[id]/index.ts
+++ pages/api/todo/[id]/index.ts
@@ -3,48 +3,55 @@ import { prisma } from "@/lib/prisma";
 
 const router = nextConnectRouter();
 
-router
-  .get(async (req, res) => {
-    const { id } = req.query;
-    const todo = await prisma.todo.findUnique({
-      where: {
-        id: id! as string,
-      },
-    });
-
-    res.json(todo);
-  })
-  .put(async (req, res) => {
-    const { id } = req.query;
-    const {
-      title = undefined,
-      description = undefined,
-      completed = undefined,
-    } = req.body;
-
-    const todo = await prisma.todo.update({
-      where: {
-        id: id! as string,
-      },
-      data: {
-        title,
-        description,
-        completed,
-      },
-    });
-
-    res.json(todo);
-  })
-  .delete(async (req, res) => {
-    const { id } = req.query;
-    const todo = await prisma.todo.delete({
-      where: {
-        id: id! as string,
-      },
-    });
-
-    res.json({ message: "success" });
+// GET
+router.get(async (req, res) => {
+  const { id } = req.query;
+  const todo = await prisma.todo.findUnique({
+    where: {
+      id: id! as string,
+    },
   });
+  if (!todo) return res.status(404).json({ message: "not found" });
+  res.json(todo);
+});
+
+// PUT
+router.put(async (req, res) => {
+  const { id } = req.query;
+  const {
+    title = undefined,
+    description = undefined,
+    completed = undefined,
+  } = req.body;
+
+  const todo = await prisma.todo.update({
+    where: {
+      id: id! as string,
+    },
+    data: {
+      title,
+      description,
+      completed,
+    },
+  });
+
+  if (!todo) return res.status(404).json({ message: "not found" });
+  res.json(todo);
+});
+
+// DELETE
+router.delete(async (req, res) => {
+  const { id } = req.query;
+  const todo = await prisma.todo.delete({
+    where: {
+      id: id! as string,
+    },
+  });
+
+  if (!todo) return res.status(404).json({ message: "not found" });
+
+  res.json({ message: "success" });
+});
 
 export default router.handler({
   onError: nextConnectErrorHandler,
